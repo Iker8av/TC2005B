2d5933303c3e460dda0497dd35948847bf90ab68
Iker8av <iker8avv@gmail.com>
2023-02-17 08:59:05 -0600

    [Unity] - Added Unity Project

---
+++ Assets/Movimiento.cs
@@ -0,0 +1,23 @@
+// Estamos usando .NET
+// Aqui "importamos" namespaces
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    // Ciclo de vida / lifecycle
+    // - Existen metodos que invocan en momentos especificos de la vida del script
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
