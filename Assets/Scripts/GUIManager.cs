a46147ddec73f0a62bb32e6fedd777ed2ce21047
Iker8av <iker8avv@gmail.com>
2023-03-03 09:24:15 -0600

    [GUI] - Added Counter

--- Assets/Scripts/GUIManager.cs
+++ Assets/Scripts/GUIManager.cs
@@ -8,18 +8,19 @@ public class GUIManager : MonoBehaviour
 {
 
     [SerializeField]
-    public TMP_Text _text; 
+    public TMP_Text _text;
+    public float contador = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        Assert.IsNotNull(_text,"Texto no puede ser nulo");
-        _text.text="Hello World";
+        Assert.IsNotNull(_text, "Texto no puede ser nulo");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _text.text = $"Puntos: {contador}";
     }
 }
